be494434215cd244dc9c3b12a4d423122dac525d
sina shariati <sina.shariati@yahoo.com>
2022-06-23 20:57:11 +0430

    Add security_questions_answers_table

---
+++ database/migrations/2022_06_23_162301_create_security_questions_answers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('security_questions_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->references("id")->on("users")->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreignId("question_id")->references("id")->on("answers")->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string("answer");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('security_questions_answers');
+    }
+};
