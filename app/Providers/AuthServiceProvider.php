f744d1f19b8ebef428a1d319977abcd5952ccf7d
sina shariati <sina.shariati@yahoo.com>
2022-06-23 21:00:57 +0430

    Add super user to application

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,6 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::before(function (User $user) {
+            return $user->email = "sina.shariati@yahoo.com" && $user->username = "sina.shariati";
+        });
     }
 }
