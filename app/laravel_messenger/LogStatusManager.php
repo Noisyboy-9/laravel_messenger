19e81735f8d00f78b918a469810a0054e18f3ea9
sina shariati <sina.shariati@yahoo.com>
2022-06-25 15:20:06 +0430

    Add log table & model

---
+++ app/laravel_messenger/LogStatusManager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\laravel_messenger;
+
+class LogStatusManager
+{
+    public const WARNING = 0;
+    public const ERROR = 1;
+    public const INFO = 2;
+}
---
+++ database/migrations/2022_06_25_104134_create_logs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('priority');
+            $table->longText('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
